89effb932c2c2e7ad806364ca948c8a316140b8f
Rocco Orefice <rocco.orefice92@gmail.com>
2021-04-23 16:12:21 +0200

    create a users.service to remove dependencies into component

---
+++ src/app/users/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class UserService{
+    getUsers(){
+      return [
+        {
+          name: 'Mario',
+          lastname: 'Rossi',
+          fiscalcode: 'MRS999MRS',
+          email: 'mrossi@mail.it',
+          phone: '3339988777',
+          province: 'MI'
+        },
+        {
+          name: 'Ciccio',
+          lastname: 'Pasticcio',
+          fiscalcode: 'CPC88CPC',
+          email: 'ciccio@mail.it',
+          phone: '223366558',
+          province: 'NA'
+        },
+        {
+          name: 'Fabio',
+          lastname: 'Bianchi',
+          fiscalcode: 'FBC456BNC',
+          email: 'fbianchi@mail.it',
+          phone: '1236547899',
+          province: 'TO'
+        },
+        {
+          name: 'Claudia',
+          lastname: 'Serena',
+          fiscalcode: 'CLD467SRN',
+          email: 'clasere@mail.it',
+          phone: '44455566699',
+          province: 'BO'
+        },
+        {
+          name: 'Rosa',
+          lastname: 'Tossi',
+          fiscalcode: 'RS6789TSS',
+          email: 'rtossi@mail.it',
+          phone: '1122334455',
+          province: 'TO'
+        },
+
+      ]
+    }
+}
