336af4ef77bbe639884889f5bfa95b55977ff659
Rocco Orefice <rocco.orefice92@gmail.com>
2021-04-23 16:49:13 +0200

    pass data from father (users) to son (user)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+
+  selector: 'tr[app-user]',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+  // decoratore Input serve per dichiarare una variabile dalla quale prendere valori
+  // 'user-data' alias della variabile user
+  @Input('user-data') user
   constructor() { }
 
   ngOnInit(): void {
