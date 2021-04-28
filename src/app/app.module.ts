858688c35f342b768a70af8c48fce6263cdea767
Rocco Orefice <rocco.orefice92@gmail.com>
2021-04-28 17:51:08 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { UserService } from './services/user.service';
 import { UsersComponent } from './users/users.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
