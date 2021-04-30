06e1a43daea2012e727edd5dcee9851f568514d6
Rocco Orefice <rocco.orefice92@gmail.com>
2021-04-30 11:41:33 +0200

    resetForm function

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { User } from '../classes/user';
+
 
 
 @Component({
@@ -18,6 +20,15 @@ export class UserDetailComponent implements OnInit {
     if (this.user.id > 0) {
       this.userService.updateUser(this.user);
 
+    } else{
+      this.userService.createUser(this.user);
+    }
+  }
+  resetForm(form){
+    if(this.user.id === 0){
+      this.user = new User();
+    } else{
+      form.reset();
     }
   }
 
